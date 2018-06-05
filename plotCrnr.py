5bf91cba55c657a5ea3294e2d9c36caf7912beb2
Andrey Danilenko <andrey.danilenko@gmail.com>
2018-06-05 08:12:09 +0300

    fitting four spectra > pn+mos1+mos2+psr+pwn_1.chain

--- plotCrnr.py
+++ plotCrnr.py
@@ -11,6 +11,10 @@ import corner
 
 samples = np.transpose(cudakde.read_data(sys.argv[1]))
 print samples.shape
+msamples = samples[:,np.r_[0:7, 13:samples.shape[1]]]
+print msamples.shape
+fsamples = msamples[np.where(msamples[:,-1]<14000),:]
+print fsamples.shape
 
-fig = corner.corner(samples[:,np.r_[0:7, 11:samples.shape[1]]], no_fill_contours=False, draw_datapoints=True)
+fig = corner.corner(msamples[np.where(msamples[:,-1]<14000)[0],:], no_fill_contours=False, draw_datapoints=True)
 fig.savefig("crnr.eps")
