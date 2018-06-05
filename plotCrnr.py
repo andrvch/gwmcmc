23507694a9445ee82335965c5eb2d4ec1bf27c5d
Andrey Danilenko <andrey.danilenko@gmail.com>
2018-06-05 03:50:48 +0300

    first clean commit to this branch

--- plotCrnr.py
+++ plotCrnr.py
@@ -12,5 +12,5 @@ import corner
 samples = np.transpose(cudakde.read_data(sys.argv[1]))
 print samples.shape
 
-fig = corner.corner(samples)
+fig = corner.corner(samples, no_fill_contours=False, draw_datapoints=True)
 fig.savefig("crnr.eps")
