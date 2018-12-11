71dcde17f17dbb3f8199b27e318a19c4e46d2287
Andrey Danilenko <andrey.danilenko@gmail.com>
2018-12-11 04:46:34 +0300

    plotOdds.py seems ok

--- plotOdds.py
+++ plotOdds.py
@@ -5,8 +5,8 @@ import os, sys
 import math
 import numpy as np
 import matplotlib
-import matplotlib.pyplot as plt
 matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 from pylab import *
@@ -18,16 +18,16 @@ nbins1D = 100
 nbins2D = 200
 
 samples = read_data(sys.argv[1])
-print samples.shape
-samples = samples[:samples.shape[0]-1,:]
+#print samples.shape
+#samples = samples[:samples.shape[0],:]
 print samples.shape
 #samples = samples[:,np.where(samples[-1,:]<14000)[0]]
 #print samples.shape
 
 #odds = - 0.5 * samples[-1] + 1717 + 1717 * math.log(9.1917e4) + ( 5. / 2. ) * math.log(9.1917e4) + 1717 * math.log(5) + 4. * math.log(2.*3.14) - 0.5*math.log(1717) - 2.84128601128261974624e1
-odds = samples[-1]
+odds = -0.5 * samples[-1]
 frqs = samples[0]
-plt.plot(np.sort(frqs),odds[np.argsort(frqs)],'o')
+plt.plot(frqs,odds,'o')
 
 oddsN = np.exp(odds)/frqs
 print oddsN.sum()
