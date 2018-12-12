c7c6db1988af4fcd5a8e7c3fb0cb8c3322916a03
Andrey Danilenko <andrey.danilenko@gmail.com>
2018-12-12 10:26:52 +0300

    nsa is working

--- simRsp.py
+++ simRsp.py
@@ -13,8 +13,8 @@ Xset.xsect = "bcmc"
 name = sys.argv[1]
 time = float(sys.argv[2])
 num = int(sys.argv[3])
-AllModels += "nsmax*phabs"
-pars = (6.0, 1.21, 1260, 1.E6, 0.15)
+AllModels += "nsa*phabs"
+pars = (6.0, 1.4, 13., 1.E12, 1.E-6, 0.15)
 AllModels(1).setPars(pars)
 for i in range(num):
     fs1 = FakeitSettings(response=name+".rmf", arf=name+".arf", fileName=name+"_"+"%1i"%(i)+".fak", exposure=time)
