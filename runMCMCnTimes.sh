927c232476532e0d3de9037b9b0280f190a1da8b
Andrey Danilenko <andrey.danilenko@gmail.com>
2018-12-12 03:56:17 +0300

    som

--- runMCMCnTimes.sh
+++ runMCMCnTimes.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 CUDAID=0
+SPECFILE1="PN_J0633_15asec_grp15.pi"
+SPECFILE2="PN_J0633_15asec_bkg.pi"
 CHAINFILE=$1
 LOGFILE="LogMetro"
 NWALK=128
@@ -9,7 +11,7 @@ NCHAINS=$3
 let NCHAINS=NCHAINS+i
 printf "Start>"
 while [ $i -lt $NCHAINS ]; do
-  ./runSFH $CUDAID $CHAINFILE $NWALK $LSTEP $i > $LOGFILE
+  ./runSFH $CUDAID $SPECFILE1 $SPECFILE1 $CHAINFILE $NWALK $LSTEP $i > $LOGFILE
   let i=i+1
   let TOTAL=i*LSTEP
   printf "$TOTAL"
