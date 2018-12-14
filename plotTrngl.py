7649d9fbf672a4a798d3fa2118af42ac7d8a67c3
Andrey Danilenko <andrey.danilenko@gmail.com>
2018-12-14 08:36:35 +0300

    som

--- plotTrngl.py
+++ plotTrngl.py
@@ -49,13 +49,59 @@ def gauss(x,y):
 zii = gauss(xii,yii)
 levi,ziin = comp_lev(zii.flatten(),quont)
 
-xi,zi = kde_gauss_cuda1d(samples[0],nbins1D)
-zin,eqh_inter[0,:] = prc(xi,zi,0.01*qlevel)
-plt.plot(xi,zin,color='blue')
-xqu = [eqh_inter[0,0],eqh_inter[0,-1],eqh_inter[0,-1],eqh_inter[0,0]]
-yqu = [zin.min(),zin.min(),zin.max()+3*(zin.max()-zin.min()),zin.max()+3*(zin.max()-zin.min())]
-plt.fill(xqu,yqu,color='0.75')
-plt.plot([eqh_inter[0,1],eqh_inter[0,1]],[zin.min(),zin.max()+3*(zin.max()-zin.min())],'--',color='black',linewidth=1.5)
+
+sttime = time.time()
+for j in range(npars):
+    for i in range(npars):
+        if i == j:
+            xi,zi = kde_gauss_cuda1d(samples[i],nbins1D)
+            zin,eqh_inter[i,:] = prc(xi,zi,0.01*qlevel)
+            ax[i,j].plot(xi,zin,color='blue')
+            xqu = [eqh_inter[i,0],eqh_inter[i,-1],eqh_inter[i,-1],eqh_inter[i,0]]
+            yqu = [zin.min(),zin.min(),zin.max()+3*(zin.max()-zin.min()),zin.max()+3*(zin.max()-zin.min())]
+            ax[i,i].fill(xqu,yqu,color='0.75')
+            ax[i,i].plot([eqh_inter[i,1],eqh_inter[i,1]],[zin.min(),zin.max()+3*(zin.max()-zin.min())],'--',color='black',linewidth=1.5)
+            zizi.append(zin)
+        elif i > j:
+            xi,yi,zi = kde_gauss_cuda2d(samples[j],samples[i],nbins2D)
+            lev,zin = comp_lev(zi,quont)
+            #ax[i,j].contourf(xi,yi,zin.reshape(xi.shape), lev, alpha=.35, cmap=plt.cm.Greens)
+            ax[i,j].contour(xi,yi,zin.reshape(xi.shape), lev, colors='blue', linewidth=.5)
+            #ax[i,j].contourf(xii,yii,ziin.reshape(xii.shape), lev, alpha=.35, cmap=plt.cm.Greens)
+            if i < npars-1:
+                ax[i,j].contour(xii,yii,ziin.reshape(xii.shape), levi, colors='black', linewidth=.5)
+
+        elif j > i:
+            ax[i,j].set_visible(False)
+print "gpu:"
+print time.time()-sttime
+
+for i in range(npars):
+    ax[i,i].set_ylabel("p.d.f.")
+    ax[i,i].yaxis.set_label_position("right")
+
+for i in range(1,npars):
+    ax[i,i].yaxis.tick_right()
+    setp([a.get_xticklabels() for a in ax[:npars-1,i]], visible=False)
+    setp([a.get_yticklabels() for a in ax[i,1:i]], visible=False)
+
+for i in range(npars):
+    for j in range(npars):
+        setp(ax[i,j].get_xticklabels(), rotation=45)
+
+ax[0,0].yaxis.tick_right()
+setp([a.get_xticklabels() for a in ax[:npars-1,0]], visible=False)
+
+for i in range(npars):
+    print eqh_inter[i,:]
+
+for j in range(npars):
+    for i in range(npars):
+        ax[i,j].set_xlim(samples[j].min()-0.001*((samples[j].max()-samples[j].min())),samples[j].max()+0.001*((samples[j].max()-samples[j].min())))
+        if i == j:
+            ax[i,j].set_ylim(zizi[i].min()+0.001*(zizi[i].max()-zizi[i].min()),zizi[i].max()+0.05*(zizi[i].max()-zizi[i].min()))
+        elif i > j:
+            ax[i,j].set_ylim(samples[i].min()-0.05*(samples[i].max()-samples[i].min()), samples[i].max()+0.05*(samples[i].max()-samples[i].min()))
 
 #plt.show()
 plt.savefig(sys.argv[1]+"trnglFrequency"+".pdf")
