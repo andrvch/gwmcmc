df9d7e14a066e365a356c0c82ebcc7a98cd0cafc
Andrey Danilenko <andrey.danilenko@gmail.com>
2019-02-19 20:13:07 +0300

    som

--- runMCMCnTimes.sh
+++ runMCMCnTimes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-CUDAID=0
+CUDAID=$1
 #SPECFILE1="pn-thin-5-ao17_0.fak"
 #SPECFILE2="pn-thin-5-ao17_1.fak"
 #SPECFILE1="PN_J0633_15asec_grp15.pi"
@@ -18,15 +18,17 @@ SPECFILE9="M1_pwn_ex_grp15.pi"
 SPECFILE10="M1_pwn_ex_bkg.pi"
 SPECFILE11="M2_pwn_ex_grp15.pi"
 SPECFILE12="M2_pwn_ex_bkg.pi"
-CHAINFILE=$1
-LOGFILE="LogGood"
+CHAINFILE="testspec_"
+LOGFILE="LogSpec"
 NWALK=128
 LSTEP=512
-i=$2
-NCHAINS=$3
-emin=$4
-emax=$5
+i=0
+NCHAINS=1
+emin=0.3
+emax=7.0
 let NCHAINS=NCHAINS+i
+printf "DeviceID=$CUDAID"
+printf "\n"
 printf "Start>"
 while [ $i -lt $NCHAINS ]; do
   ./runSFH $CUDAID $SPECFILE1 $SPECFILE2 $SPECFILE3 $SPECFILE4 $SPECFILE5 $SPECFILE6 $SPECFILE7 $SPECFILE8 $SPECFILE9 $SPECFILE10 $SPECFILE11 $SPECFILE12 $CHAINFILE $NWALK $LSTEP $i $emin $emax > $LOGFILE
