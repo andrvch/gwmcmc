d97403dc451387bdd45512250f8150741a167437
Andrey Danilenko <andrey.danilenko@gmail.com>
2019-02-26 17:20:04 +0300

    minor corrections: delete some comments

--- StrctrsAndFnctns.cu
+++ StrctrsAndFnctns.cu
@@ -202,9 +202,7 @@ __global__ void updateWalkers ( const int dim, const int nwl, const float *xx1, 
   int j = threadIdx.y + blockDim.y * blockIdx.y;
   int t = i + j * dim;
   if ( i < dim && j < nwl ) {
-    //if ( q[j] > r[j] ) {
     xx0[t] = ( q[j] > r[j] ) * xx1[t] + ( q[j] <= r[j] ) * xx0[t];
-    //}
   }
 }
 
