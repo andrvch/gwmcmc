2bc6c7dadb1a8a64cd63607b3d8f620b9e261648
Andrey Danilenko <andrey.danilenko@gmail.com>
2019-10-14 17:34:04 +0300

    edit list_fluxes.py, printCrdblIntrvls.py; add printCrdblIntrvlsBB.py

--- list_fluxes.py
+++ list_fluxes.py
@@ -16,7 +16,7 @@ def FoldInput(infile):
         pp = read_data(line.split()[0])
         pars.append(pp)
     prst = []
-    for j in range(10):
+    for j in range(7):
         lst = []
         for i in range(len(lines)):
             lst.append(r'$%1.2f^{+%1.2f}_{-%1.2f}$'%(pars[i][1,j],pars[i][2,j]-pars[i][1,j],pars[i][1,j]-pars[i][0,j]))
@@ -27,5 +27,5 @@ prst = FoldInput(sys.argv[1])
 
 models = ['bb','nsa12','nsa13','ns1260','ns123100','ns123190','ns130100','ns130190']
 
-data = {r'A$Mod.$': models, r'B$N_{\rm H}$': prst[6], r'F$\Gamma_{\rm psr}$': prst[2], r'G$K_{\rm psr}$': prst[3], r'C$T$': prst[0], r'D$R$': prst[1], r'E$D$': prst[7], r'H$\Gamma_{\rm pwn}$': prst[4], r'J$K_{\rm pwn}$': prst[5], r'K$W$': prst[8], r'K$\chi^{2}$': prst[9]}
-asciitable.write(data, sys.stdout, Writer = asciitable.Latex, latexdict = {'preamble': r'\begin{center}', 'tablefoot': r'\end{center}', 'tabletype': 'table*', 'units':{'$N_{\rm H}$':'$\rm 10^{21} ./ cm^{-2}$'}})
+data = {r'A $Mod.$': models, r'B Bol. lum.': prst[0], r'C $Psr. flux$': prst[1], r'D PWN flux': prst[2], r'E Psr. Lum.': prst[3], r'F PWN Lum': prst[4], r'G Psr. eff.': prst[5], r'H PWN eff': prst[6]}
+asciitable.write(data, sys.stdout, Writer = asciitable.Latex, latexdict = {'preamble': r'\begin{center}', 'tablefoot': r'\end{center}', 'tabletype': 'table*')
