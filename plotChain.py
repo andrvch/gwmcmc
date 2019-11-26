9cb20b1c25e7d767c778f88e70adcd8392055a16
Andrey Danilenko <andrey.danilenko@gmail.com>
2019-11-26 14:48:17 +0300

    add plotChain.py

--- plotChain.py
+++ plotChain.py
@@ -1,26 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import os, sys
 import math
 import numpy as np
 import matplotlib
-#matplotlib.use('Agg')
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from pylab import *
 from cudakde import *
 
-samples = read_data(sys.argv[1])
-print samples.shape
-#exit()
-#samples = samples[:samples.shape[0],:]
-samples = samples[np.r_[0:7, samples.shape[0]-1],:]
-print samples.shape
-#samples = samples[:,np.where(samples[-1,:]<14000)[0]]
+smpls = read_data(sys.argv[1]+"%i"%(int(sys.argv[2]))+".chain")
+nwlkrs = int(sys.argv[3])
 
-nwlkrs = int(sys.argv[2])
-nprmtrs = shape(samples)[0]
-nstps = int(shape(samples)[1]/float(nwlkrs))
+nprmtrs = shape(smpls)[0]
+nstps = int(shape(smpls)[1]/float(nwlkrs))
 
 print nstps, nprmtrs
 
@@ -29,7 +26,7 @@ wlkrs = np.empty([nprmtrs,nwlkrs,nstps])
 for i in range(nstps):
     for j in range(nwlkrs):
         for k in range(nprmtrs):
-            wlkrs[k,j,i] = samples[k,j+nwlkrs*i]
+            wlkrs[k,j,i] = smpls[k,j+nwlkrs*i]
 
 stps = linspace(1,nstps,num=nstps)
 fig, ax = plt.subplots(nrows=nprmtrs)
@@ -39,5 +36,5 @@ for i in range(nprmtrs):
     for j in range(nwlkrs):
         ax[i].errorbar(stps,wlkrs[i,j,:])
 
-plt.show()
-#plt.savefig(sys.argv[1]+".jpg")
+#plt.show()
+plt.savefig(sys.argv[1]+"%i"%(int(sys.argv[2]))+"_chain"+".jpg")
