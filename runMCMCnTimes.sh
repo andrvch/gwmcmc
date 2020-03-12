db410d8a2480118fc1f2190f9afc4a02e1d52e83
Andrey Danilenko <andrey.danilenko@gmail.com>
2020-03-12 21:47:45 +0300

    edit runMCMCnTimes.sh

--- runMCMCnTimes.sh
+++ runMCMCnTimes.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 CUDAID=1
-SPECFILE1="PN_J0633_15asec_grp1.pi"
-SPECFILE2="PN_J0633_15asec_bkg.pi"
-SPECFILE3="M1_J0633_15asec_grp1.pi"
-SPECFILE4="M1_J0633_bkg.pi"
-SPECFILE5="M2_J0633_15asec_grp1.pi"
-SPECFILE6="M2_J0633_15asec_bkg.pi"
-SPECFILE7="PN_pwn_ex_grp1.pi"
-SPECFILE8="PN_pwn_ex_grp_bkg.pi"
-SPECFILE9="M1_pwn_ex_grp1.pi"
-SPECFILE10="M1_pwn_ex_grp_bkg.pi"
-SPECFILE11="M2_pwn_ex_grp1.pi"
-SPECFILE12="M2_pwn_ex_grp_bkg.pi"
+SPECFILE1="M1_psr12asec_grp1.pi"
+SPECFILE2="M2_psr12asec_grp1.pi"
+SPECFILE3="PN_psr12asec_grp1.pi"
 CHAINFILE=$1
 LOGFILE=$2
 NWALK=128
@@ -25,10 +16,10 @@ printf "DeviceID=$CUDAID"
 printf "\n"
 printf "Start>"
 while [ $i -lt $NCHAINS ]; do
-  ./runSFH $CUDAID $SPECFILE1 $SPECFILE2 $SPECFILE3 $SPECFILE4 $SPECFILE5 $SPECFILE6 $SPECFILE7 $SPECFILE8 $SPECFILE9 $SPECFILE10 $SPECFILE11 $SPECFILE12 $CHAINFILE $NWALK $LSTEP $i $emin $emax > $LOGFILE
+  ./runSFH $CUDAID $SPECFILE1 $SPECFILE2 $SPECFILE3 $CHAINFILE $NWALK $LSTEP $i $emin $emax > $LOGFILE
   ./plotChain.py $CHAINFILE $i $NWALK
-  ./printSpecStat.py $CHAINFILE
-  ./plotSpectraFromFile.py $CHAINFILE
+  #./printSpecStat.py $CHAINFILE
+  #./plotSpectraFromFile.py $CHAINFILE
   ./printCrdblFromFile.py $CHAINFILE
   let i=i+1
   let TOTAL=i*LSTEP
