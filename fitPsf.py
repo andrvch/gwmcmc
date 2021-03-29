19d10517d401313bbc9e5a95c2fb8656974363db
Andrey Danilenko <andrey.danilenko@gmail.com>
2021-03-29 21:05:00 +0300

    edit fitPsf.py

--- fitPsf.py
+++ fitPsf.py
@@ -13,67 +13,74 @@ from astropy.io import fits
 import numpy as np
 import pyregion
 import re
+#from cudakde import *
 
-fitsfls = [sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7], sys.argv[8], sys.argv[9]] # (files position_input or position_input_11)
+fl = sys.argv[1]
 
-for fl in fitsfls:
-    hdul = fits.open(fl)
-    #hdul.info()
+hdul = fits.open(fl)
+hdr = hdul[0].header
+#print(repr(hdr))
 
-    hdr = hdul[0].header
-    #print(repr(hdr))
-    xrf = hdr['CRVAL1P']
-    yrf = hdr['CRVAL2P']
-    xscl = hdr['CDELT1P']
-    yscl = hdr['CDELT2P']
-    print(xrf,yrf)
-    print(xscl,yscl)
+xrf = hdr['CRVAL1P']
+yrf = hdr['CRVAL2P']
+xscl = hdr['CDELT1P']
+yscl = hdr['CDELT2P']
+print(xrf,yrf)
+print(xscl,yscl)
 
-    data = hdul[0].data
-    ny = shape(data)[0]
-    nx = shape(data)[1]
-    print(nx,ny)
-    f = open(fl+".psf", "w")
-    f.write("%.13E\n"%(xrf))
-    f.write("%.13E\n"%(yrf))
-    f.write("%.18E\n"%(xscl))
-    f.write("%.18E\n"%(yscl))
-    for j in range(ny):
-        for i in range(nx):
-            f.write("%.15E "%(data[j,i]))
-        f.write("\n")
-    f.close()
+data = hdul[0].data
 
+ny = shape(data)[0]
+nx = shape(data)[1]
+print(nx,ny)
 
-exit()
+f = open(fl+".psf", "w")
 
+f.write("%.13E\n"%(xrf))
+f.write("%.13E\n"%(yrf))
+f.write("%.18E\n"%(xscl))
+f.write("%.18E\n"%(yscl))
 
-#data.field(0)
-#for i in range(len(data)):
-#    print(data[i])
-
-#clms = hdul[1].columns
-#cnms = clms.names
-
-#n1 = len(cnms)
-#n2 = len(data)
+for j in range(ny):
+    for i in range(nx):
+        f.write("%.15E "%(data[j,i]))
+    f.write("\n")
+f.close()
 
 
-#print(clms.names)
+xii,yii = np.mgrid[1.:nx:nx*1j,1.:ny:ny*1j]
+print(xii)
+print(data)
+
+def comp_lev(zi,quont):
+    zisort = np.sort(zi)[::-1]
+    zisum = np.sum(zi)
+    zicumsum = np.cumsum(zisort)
+    zicumsumnorm = zicumsum/zisum
+    #print(zicumsumnorm)
+    #print(np.where(zicumsumnorm>90)[0])
+    zinorm = zi/zisum
+    zisortnorm = zisort/zisum
+    levels = [zisortnorm[np.where(zicumsumnorm>qu)][0] for qu in quont]
+    return levels,zinorm,zisum
 
 
-for i in range(n2):
-    print(data['X'][i],data['X_ERR'][i],data['Y_ERR'][i])
+quont = [0.90,0.68,0.40]
+lev,zin,zisum = comp_lev(data.flatten(),quont)
 
-for i in range(n1):
-    print(cnms[i],data[0][i])
+print(zisum)
+print(lev)
 
-print(np.floor(pars[0,:,0]))
-for i in range(n2):
-    res = np.where(np.floor(pars[0,:,0])==math.floor(data['X'][i]))
-    if len(res[0]) > 0:
-        print(res[0][0],math.floor(data['X'][i]))
+for i in range(len(quont)):
+    print(lev[i]*zisum)
 
+plt.contour(xii,yii,zin.reshape(xii.shape), lev, colors='blue', linewidth=.5)
 
+plt.savefig(sys.argv[1]+"cntr"+".png")
 
-#print(hdul[1].data)
+"""
+def gauss(x,y):
+    return np.exp(-0.5*(x**2+y**2))
+zii = gauss(xii,yii)
+levi,ziin = comp_lev(zii.flatten(),quont)
+"""
