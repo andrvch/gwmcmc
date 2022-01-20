d85e5552579498945e752b6541e864ecb9ab8612
Andrey Danilenko <andrey.danilenko@gmail.com>
2022-01-20 21:43:08 +0300

    add fig_3_1.py

---
+++ fig_3_1.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os, sys
+import time
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import math
+from pylab import *
+from astropy.io import ascii
+from astropy.io import fits
+import numpy as np
+#import pyregion
+#import re
+
+fl = "spC.fits"
+
+hdul = fits.open(fl)
+print(shape(hdul)[0])
+f = open(fl+".spC", "w")
+
+for i in range(shape(hdul)[0]):
+    print("%i:"%i)
+    hdr = hdul[i].header
+    #print(repr(hdr))
+    data = hdul[i].data
+    print(shape(data))
+    if i < 5:
+        print(data)
+    if i == 1:
+        for j in range(shape(data)[0]):
+            for k in range(2):
+                f.write(" %.15E "%(data[j][k]))
+        f.write("\n")
+    if i == 2 or i == 3:
+        for j in range(shape(data)[0]):
+            f.write(" %.15E "%(data[j][0]))
+        f.write("\n")
+    if 3 < i:
+        #for k in range(61):
+        #    for j in range(1000):
+        #        f.write(" %.15E "%(data[0][k][j]))
+        f.write("\n")
+
+f.close()
