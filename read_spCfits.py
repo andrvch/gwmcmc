edea7c4b2c786b4925dcca5caa7b3c65629c9864
Andrey Danilenko <andrey.danilenko@gmail.com>
2022-08-16 17:41:19 +0200

    edit read_spCfits.fits; sims done

--- read_spCfits.py
+++ read_spCfits.py
@@ -26,21 +26,26 @@ for i in range(shape(hdul)[0]):
     #print(repr(hdr))
     data = hdul[i].data
     print(shape(data))
-    if i < 5:
-        print(data)
+    #if i < 5:
+    #print(data)
     if i == 1:
         for j in range(shape(data)[0]):
             for k in range(2):
                 f.write(" %.15E "%(data[j][k]))
+            f.write("\n")
         f.write("\n")
     if i == 2 or i == 3:
         for j in range(shape(data)[0]):
             f.write(" %.15E "%(data[j][0]))
         f.write("\n")
-    if 3 < i:
-        #for k in range(61):
-        #    for j in range(1000):
-        #        f.write(" %.15E "%(data[0][k][j]))
+        f.write("\n")
+    if 4 == i:
+        #print(data)
+        print(type(data))
+        for k in range(61):
+            for j in range(1000):
+                f.write(" %.15E "%(data[k][0][j]))
+            f.write("\n")
         f.write("\n")
 
 f.close()
