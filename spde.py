f46db8fca838ea6cc9e22eb862efe2375bef285b
Andrey Danilenko <andrey.danilenko@gmail.com>
2023-06-11 13:56:33 +0530

    no errors; seems like cupy and torch are installed; still cannot install pycuda though

--- spde.py
+++ spde.py
@@ -1,9 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import pycuda.autoinit
-import pycuda.driver as drv
-import numpy
+#import pycuda.autoinit
+#import pycuda.driver as drv
+import numpy as np
 import torch
+import cupy as cp
 
 x = torch.rand(5, 3)
